7c8846685ff181588def995e6d6a49a1fc29eebf
Joel Gutierrez <gutierrj@usc.edu>
2018-06-26 18:05:50 -0700

    main final code

--- tip_calculator/tip_calculator/ViewController.m
+++ tip_calculator/tip_calculator/ViewController.m
@@ -21,6 +21,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [_billField becomeFirstResponder];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
